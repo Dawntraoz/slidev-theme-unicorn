8835e8906adb0449679b158fd11466d0ed4d8d02
HipsterBrown <headhipster@hipsterbrown.com>
2022-05-19 13:18:24 -0400

    fix: resolve public image url for static hosting

--- layouts/image-center.vue
+++ layouts/image-center.vue
@@ -2,13 +2,15 @@
   <default class="image-center">
     <div class="flex flex-col">
       <slot />
-      <img :src="image" alt="Slide image" :width="imageWidth" :height="imageHeight" class="rounded-md shadow-lg  m-auto object-contain" />
+      <img :src="imageSrc" alt="Slide image" :width="imageWidth" :height="imageHeight"
+        class="rounded-md shadow-lg  m-auto object-contain" />
     </div>
   </default>
 </template>
 
 <script setup lang="ts">
-import { defineProps, defineComponent } from 'vue'
+import { defineProps, defineComponent, computed } from 'vue'
+import { resolveAssetUrl } from '@slidev/client/layoutHelper'
 import Default from '../layouts/default.vue'
 
 const components = defineComponent({ Default })
@@ -23,4 +25,5 @@ const props = defineProps({
     type: String,
   },
 })
+const imageSrc = computed(() => resolveAssetUrl(props.image))
 </script>
--- layouts/intro.vue
+++ layouts/intro.vue
@@ -2,7 +2,8 @@
   <default class="intro" :logoHeader="logoHeader">
     <div class="flex items-center">
       <figure class="w-1/2 px-12">
-        <img :src="introImage" height="312" width="312" class="rounded-full p-2 bg-gradient-to-r from-fuchsia-700 to-purple-800 dark:(from-white to-purple-50)">
+        <img :src="imageSrc" height="312" width="312"
+          class="rounded-full p-2 bg-gradient-to-r from-fuchsia-700 to-purple-800 dark:(from-white to-purple-50)">
       </figure>
       <div class="w-1/2">
         <slot />
@@ -12,7 +13,8 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, defineComponent } from 'vue'
+import { defineProps, defineComponent, computed } from 'vue'
+import { resolveAssetUrl } from '@slidev/client/layoutHelper'
 import Default from '../layouts/default.vue'
 
 const components = defineComponent({ Default })
@@ -21,4 +23,5 @@ const props = defineProps({
     type: String,
   },
 })
+const imageSrc = computed(() => resolveAssetUrl(props.introImage))
 </script>
